56cbb34527572a783875f708f74be909c6bc264a
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2015-06-30 23:54:56 +0200

    Added celery application object and changed the command to run the task

--- youtuber/management/commands/youtube_start.py
+++ youtuber/management/commands/youtube_start.py
@@ -1,11 +1,7 @@
 from django.core.management.base import BaseCommand
-from youtuber.models import YoutubeSource
-from youtuber.tasks import youtuber_scrape
+from youtuber.tasks import youtuber_start
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        youtube_sources = YoutubeSource.objects.all()
-
-        for source in youtube_sources:
-            youtuber_scrape(source_id=source.id)
+        youtuber_start.delay()
